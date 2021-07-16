cc5185f6e9b020b7d66dff7708d580bec7e6ec04
Oleg <oleg_kovtkov@mail.ru>
2021-07-16 15:14:22 +0300

    fix

--- source/components/navigation/navigation.js
+++ source/components/navigation/navigation.js
@@ -1,5 +1,26 @@
 'use strict';
 
-let navigation = document.querySelector('.navigation');
-let close = document.querySelector('.navigation__close');
-close.addEventListener('click', () => navigation.classList.remove('navigation_open'));
+class Menu {
+    constructor() {
+        this.burger = document.querySelector('.header__burger');
+        this.navigation = document.querySelector('.navigation');
+        this.closeButton = document.querySelector('.navigation__close');
+
+        this.burger.addEventListener('click', () => this.open());
+        this.closeButton.addEventListener('click', () => this.close());
+        document.addEventListener('click', (event) => {
+            if (!event.target.closest('.navigation') && event.target !== this.burger) 
+                this.close();
+        });
+    }
+
+    close() {
+        this.navigation.classList.remove('navigation_open');
+    }
+
+    open() {
+        this.navigation.classList.add('navigation_open');
+    }
+}
+
+new Menu();
--- source/javascript/main.js
+++ source/javascript/main.js
@@ -1,4 +1,3 @@
 import "../components/header/header.scss";
-import "../components/header/header.js";
 import "../components/navigation/navigation.scss";
 import "../components/navigation/navigation.js";
